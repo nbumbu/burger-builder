e4d484d0b7afa008d154757aa492d3454056dc97
Nick <sahas023@gmail.com>
2019-08-20 18:18:13 +0200

    Replaced AsyncComponent HOC with React lazy

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
-import React, { Component } from 'react';
+import React, {Suspense} from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import asyncComponent from './hoc/asyncComponent/asyncComponent';
 
-const asyncCheckout = asyncComponent(() => {
+const Checkout = React.lazy(() => {
   return import('./containers/Checkout/Checkout')
 })
 
-const asyncOrders = asyncComponent(() => {
+const Orders = React.lazy(() => {
   return import('./containers/Orders/Orders')
 })
 
 
-class App extends Component {
-
-  render() {
-    return (
-      <div>
-        <Layout>
-          <Switch>
-            <Route path="/checkout" component={asyncCheckout} />
-            <Route path="/orders" component={asyncOrders} />
-            <Route path="/" component={BurgerBuilder} />
-          </Switch>
-        </Layout>
-      </div>
-    )
-  }
-
+const App = (props) => {
+  return (
+    <div>
+      <Layout>
+        <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path="/checkout" render={() => <Checkout/>} />
+          <Route path="/orders" render={() => <Orders/>} />
+          <Route path="/" component={BurgerBuilder} />
+        </Switch>
+        </Suspense>
+      </Layout>
+    </div>
+  )
 }
 
 export default App;
